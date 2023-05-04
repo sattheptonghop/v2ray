b91548a2c706af1b7617f3a3ec8a33ef72aa3fa8
sattheptonghop <tuananh88c25@gmail.com>
2023-05-04 12:30:25 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.service import Service
+
+def test_google_search():
+    # Khởi tạo Chrome Driver Service
+    service = Service("/usr/local/bin/chromedriver")
+    service.start()
+    # Khởi tạo trình duyệt Chrome
+    driver = Chrome(service=service)
+    # Mở trang Google
+    driver.get('https://www.google.com')
+    # Tìm ô tìm kiếm và nhập từ khóa "GitHub"
+    search_box = driver.find_element_by_name('q')
+    search_box.send_keys('GitHub')
+    search_box.submit()
+    # Kiểm tra xem kết quả có chứa từ khóa "GitHub" không
+    assert 'GitHub' in driver.title
+    # Đóng trình duyệt
+    driver.quit()
+
+test_google_search()
