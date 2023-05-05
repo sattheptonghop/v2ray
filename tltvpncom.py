bd82d07d4a685ba431562b65e12b4fd8d429db4f
sattheptonghop <tuananh88c25@gmail.com>
2023-05-05 11:17:43 +0700

    Update tltvpncom.py

--- tltvpncom.py
+++ tltvpncom.py
@@ -59,7 +59,7 @@ while re.search(r"/#/(.*)",driver.current_url).group(1) != "dashboard":
 	if re.search(r"/#/(.*)",driver.current_url).group(1) == "login":
 		print(driver.current_url)
 		print('Thu chuyen toi trang dang ky')
-		driver.save_screenshot("tltvpncom-" + str(iLoop) + "tcttdk.png")
+		driver.save_screenshot("tltvpncom/" + str(iLoop) + "tcttdk.png")
 		try:
 			#element = driver.find_element(By.LINK_TEXT, "Đăng ký")
 			element = driver.find_element(By.XPATH, "//main[@id=\'main-container\']/div/div/div/div[2]/div/form/div[2]/p/a[2]")
@@ -118,14 +118,14 @@ while re.search(r"/#/(.*)",driver.current_url).group(1) != "dashboard":
 						print('dong thong bao')
 					except:
 						pass
-					driver.save_screenshot("tltvpncom-" + str(iLoop) + "nhapxong.png")
+					driver.save_screenshot("tltvpncom/" + str(iLoop) + "nhapxong.png")
 				except Exception as e:
 					print("loi khi nhap email, pass")
 					print(e)
 					pass
 
 			print('thu an nut dang ky')
-			driver.save_screenshot("tltvpncom-" + str(iLoop) + "tandk.png")
+			driver.save_screenshot("tltvpncom/" + str(iLoop) + "tandk.png")
 			try:
 				element = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//main[@id=\'main-container\']/div/div/div/div/div[2]/div/div[2]/button")))
 				#element = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-block")))
@@ -154,7 +154,7 @@ while re.search(r"/#/(.*)",driver.current_url).group(1) != "dashboard":
 		except Exception as e:
 			print("khong co iemail")
 			print(e)
-			driver.save_screenshot("tltvpncom-" + str(iLoop) + "kci.png")
+			driver.save_screenshot("tltvpncom/" + str(iLoop) + "kci.png")
 			pass
 		##print("Bat dau Chuyển lai iframe")
 		##print(driver.current_url)
@@ -236,6 +236,6 @@ if re.search(r"/#/(.*)",driver.current_url).group(1) == "dashboard":
 
 	
 # Đóng trình duyệt web
-driver.save_screenshot("tltvpncom-10.png")
+driver.save_screenshot("tltvpncom/10.png")
 driver.close()
 driver.quit()
