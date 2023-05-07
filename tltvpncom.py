180dcac6af0dad58f33a8bf48cae6a8c545fd95c
sattheptonghop <tuananh88c25@gmail.com>
2023-05-07 15:08:16 +0700

    Update tltvpncom.py

--- tltvpncom.py
+++ tltvpncom.py
@@ -18,6 +18,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from chromedriver_binary import chromedriver_binary_path
+
 chrome_service = Service(ChromeDriverManager().install())
 chrome_options = Options()
 options = [
@@ -39,7 +41,7 @@ mobile_emulation = {
     "userAgent": "Mozilla/5.0 (Android 12; Mobile; LG-M255; rv:100.0) Gecko/100.0 Firefox/100.0Mozilla/5.0 (Linux; Android 12) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.61 Mobile Safari/537.36"
 }
 chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
-driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+driver = webdriver.Chrome(executable_path=chromedriver_binary_path, service=chrome_service, options=chrome_options)
 
 # 1 | setWindowSize | 500x1200 | 
 driver.set_window_size(360, 720)
@@ -130,18 +132,16 @@ while re.search(r"/#/(.*)",driver.current_url).group(1) != "dashboard":
 				#element = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//main[@id=\'main-container\']/div/div/div/div/div[2]/div/div[2]/button")))
 				#element = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-block")))
 				try:
-					WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".custom-control-label"))).click()
+					driver.find_element(By.CSS_SELECTOR, ".custom-control-label").click()
 				except:
 					print('ko an duoc nut dong y dieu kien')
 					pass
 				try:
-					btn = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-block")))
-					btn.click()
+					driver.find_element(By.CSS_SELECTOR, ".btn-block").click()
 				except TimeoutException:
 					print('Không tìm thấy button có class là "btn-block"')
 					try:
-						btn = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".fa-fw")))
-						btn.click()
+						driver.find_element(By.CSS_SELECTOR, ".fa-fw").click()
 					except TimeoutException:
 						print('Không tìm thấy button có class là ".fa-fw"')
 					except Exception as ex:
